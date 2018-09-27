70adc84aed3ccc956c231d2e94b3d03031b97dbd
Liron193 <liron@probot.ai>
2018-09-27 21:24:22 +0300

    Added connect and disconectevent handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 var path = require('path');
+const http = require('http');
 var express = require('express');
+var socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+// Listen to event of user connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    // Listen to user disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected from server');
+    });
+});
+
 
 // Middleware to serve public directory
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
-})
+});
